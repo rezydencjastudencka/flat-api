25dee5ead0ab7e62d03cadc4e550fc8a9c94b843
Marcin Moskal <mmos@memleak.pl>
2018-02-06 22:06:54 +0100

    Build standalone image

--- app/flat_api_django/local.py
+++ app/flat_api_django/local.py
@@ -1,7 +1,5 @@
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'k#xfum2s88z=a++od3x66c5gzk&c3qqd-_g1%&vuxeyh&vxi-n'
 
@@ -21,6 +19,6 @@ CORS_ORIGIN_WHITELIST = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': '/db.sqlite3',
     }
 }
