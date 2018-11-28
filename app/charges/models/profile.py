23d665d62e8d70eaf0453729cefa7282391664f2
Mateusz "maxmati" Nowotyński <maxmati4@gmail.com>
2018-11-28 23:15:07 +0100

    Export basic metrics to /metrics endpoint
    Signed-off-by: Mateusz "maxmati" Nowotyński <maxmati4@gmail.com>

--- app/charges/models/profile.py
+++ app/charges/models/profile.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django_prometheus.models import ExportModelOperationsMixin
 
 from charges.models.flat import Flat
 
 
-class Profile(models.Model):
+class Profile(ExportModelOperationsMixin('profile'), models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     flat = models.ForeignKey(Flat, on_delete=models.PROTECT)
